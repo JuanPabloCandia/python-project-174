728742b6a0454ec7d9c60b5f11ea4715909b6bb7
Juan Pablo andia Tavera <jcandiatavera@gmail.com>
2025-05-05 14:43:33 -0500

    gendiff

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -1,8 +1,8 @@
-import argparse
-from gendiff.scripts.gendiff import generate_diff
+import argparse 
+from gendiff import generate_diff
 
 
-def parse_args():
+def main():
     parser = argparse.ArgumentParser(
         description="Compares two configuration files and shows a difference."
     )
@@ -11,10 +11,10 @@ def parse_args():
     parser.add_argument(
         "-f", "--format", default="stylish", help="set format of output"
     )
-    return parser.parse_args()
-
-
-def main():
-    args = parse_args()
+    args = parser.parse_args()
     diff = generate_diff(args.first_file, args.second_file, args.format)
     print(diff)
+
+
+if __name__ == "__main__":
+    main()
